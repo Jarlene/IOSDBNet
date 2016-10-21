620c07b47ac6cde9eee3677b22dc78dbcb7ce83b
魔才 <jarlene.xwl@alibaba-inc.com>
2016-10-21 16:38:55 +0800

    新建网络库。

---
+++ HttpClientSample/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  HttpClientSample
+//
+//  Created by Jarlene on 2016/10/21.
+//  Copyright © 2016年 Jarlene. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+/**
+ * 基础ViewController
+ * 将在此Controller中
+ * 封装网络请求，网络变化，刷新数据等功能，数据库变化等功能。
+ *
+ */
+class BaseViewController: UIViewController {
+    
+}
--- HttpClientSample/ViewController.swift
+++ HttpClientSample/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import NetJson
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
 
+    @IBOutlet weak var urlEdit: UITextField!
+    @IBOutlet weak var semdButton: UIButton!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
+    /**
+     * 从网络拉取数据，之后通知到主线程
+     * - Parameter request: 请求参数
+     * - Parameter resopnse: 回调闭包
+     * - Returns: void
+     */
+    func getData(request: Request?, resopnse: @escaping (Responese)-> Void )  {
+        if let request = request {
+            let httpClient = HttpClient();
+            httpClient.send(request: request, response: resopnse)
+        }
+    }
 
+    @IBAction func requestAction(_ sender: AnyObject) {
+        let url = urlEdit.text
+        let request = Request(url: url!, type: .Get)
+        getData(request: request, resopnse: { res in
+            if let result = res.result {
+                self.result.text =  result
+            }
+ 
+        })
+    }
 }
 
